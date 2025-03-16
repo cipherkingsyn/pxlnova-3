ef604f373d81a3e8be0269514843ca9363b5f703
cipherkingsyn <160332792+cipherkingsyn@users.noreply.github.com>
2025-03-16 19:25:44 -0400

    Update uv.config.js

--- web/uv/uv.config.js
+++ web/uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/web/uv/service/',
-    bare: 'https://bare-sigma.yourmom.us.kg/',
+    bare: 'https://v2202412246404304352.megasrv.de/bare/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/web/uv/uv.handler.js',
